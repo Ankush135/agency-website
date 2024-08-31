967411bb47421b2c192d8ce0093c9d76924cc58d
ankush <ankushkashyap284@gmail.com>
2024-09-01 00:30:41 +0530

    all changed

---
+++ script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var swiper = new Swiper('.swiper-container', {
+        loop: true,
+        slidesPerView: 1,
+        spaceBetween: 10,
+        centeredSlides: true,
+        autoplay: {
+            delay: 3000,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        speed: 600, // Adjust the speed as needed
+    });
+});
